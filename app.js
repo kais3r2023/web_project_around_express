944d5cc10c50641e9ab0d51b82e05f645b7406de
kais3r2023 <122757568+kais3r2023@users.noreply.github.com>
2024-04-09 13:04:05 -0500

    proyecto-sprint-13-corregido

--- app.js
+++ app.js
@@ -2,18 +2,17 @@ const express = require('express');
 const users = require('./routes/users');
 const cards = require('./routes/cards');
 
-
 const app = express();
-const {PORT = 3000} = process.env;
+const { PORT = 3000 } = process.env;
 
 app.use('/users', users);
 app.use('/cards', cards);
 
 // Ruta por defecto: devuelve un mensaje de error para cualquier otra ruta
-app.use((req, res)=>{
-    res.status(404).json({ message: 'Recurso solicitado no encontrado'});
-})
+app.use((req, res) => {
+  res.status(404).json({ message: 'Recurso solicitado no encontrado' });
+});
 
-app.listen(PORT, ()=>{
+app.listen(PORT, () => {
   console.log(` Servidor en ejecución en http://localhost:${PORT}`);
-})
+});
